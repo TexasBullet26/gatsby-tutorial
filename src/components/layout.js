d49d1b8b55eee44af596438bb3cbe7d1d9c0b519
Glenn Lanzer <lanzertrey@gmail.com>
2018-09-21 14:45:10 -0500

    Add: StaticQuery -> components/layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,66 @@
 import React from 'react'
 import { css } from 'react-emotion'
-import { Link } from 'gatsby'
+import { StaticQuery, Link, graphql } from 'gatsby'
 
 import { rhythm } from '../utils/typography'
 
 export default ({ children }) => (
-    
-    <div
-        className={css`
-            margin: 0 auto;
-            max-width: 700px;
-            padding: ${rhythm(2)};
-            padding-top: ${rhythm(1.5)};
-        `}
-    >
 
-        <Link to={`/`}>
-            
-            <h3
+    <StaticQuery
+        query={graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+        `
+    }
+
+        render={data => (
+    
+            <div
                 className={css`
-                    margin-bottom: ${rhythm(2)};
-                    display: inline-block;
-                    font-style: normal;
+                    margin: 0 auto;
+                    max-width: 700px;
+                    padding: ${rhythm(2)};
+                    padding-top: ${rhythm(1.5)};
                 `}
             >
+
+                <Link to={`/`}>
+            
+                    <h3
+                        className={css`
+                            margin-bottom: ${rhythm(2)};
+                            display: inline-block;
+                            font-style: normal;
+                        `}
+                    >
                 
-                Pandas Eating Lots
+                    {data.site.siteMetadata.title}
             
-            </h3>
+                    </h3>
         
-        </Link>
+                </Link>
         
-        <Link
-            to={`/about/`}
-            className={css`
-                float: right;
-            `}
-        >
+                <Link
+                    to={`/about/`}
+                    className={css`
+                        float: right;
+                    `}
+                >
             
-            About
+                    About
         
-        </Link>
+                </Link>
         
-        {children}
+                {children}
     
-    </div>
+            </div>
+        )}
+
+    />
 
 )
